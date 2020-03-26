902395e419131947aae2337b50261a37cd43a853
abhijeet gupta <abhijeetg40@gmail.com>
2020-03-26 12:24:31 +0530

    db

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/data'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/data'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'corona_api' 
